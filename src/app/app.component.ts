7368313ca1910ba7a2cffa93f46f27649499dc47
DownloadableFox <me@downloadablefox.dev>
2025-04-01 09:51:31 -0500

    now using switch maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { User } from './models/user.model';
 import { Post } from './models/post.model';
 import { Comment } from './models/comment.model';
+import { catchError, forkJoin, map, of, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -25,36 +26,48 @@ export class AppComponent {
     this.comments = {};
     this.errorMessage = '';
 
-    this.http.get<{ users: User[] }>(`${this.ROOT_URL}/users/filter?key=username&value=${this.username}`).subscribe(
-        (response) => {
+    this.http
+      .get<{ users: User[] }>(`${this.ROOT_URL}/users/filter?key=username&value=${this.username}`)
+      .pipe(
+        switchMap((response) => {
           if (response.users.length > 0) {
             this.user = response.users[0];
-            this.getPost(this.user.id);
+            return this.getPosts(this.user.id);
           } else {
-            this.errorMessage = 'No existe  ';
+            this.errorMessage = 'No existe';
+            return of([]); // Return an empty array if no user is found
           }
+        }),
+        catchError((error) => {
+          this.errorMessage = 'An error occurred while fetching user data.';
+          return of([]); // Handle errors gracefully
+        })
+      )
+      .subscribe((posts) => {
+        this.posts = posts;
+        if (posts.length > 0) {
+          this.getCommentsForPosts(posts);
         }
-      );
+      });
   }
 
-  getPost(userId: number) {
-    this.http.get<{ posts: Post[] }>(`${this.ROOT_URL}/posts/user/${userId}`).subscribe(
-      (response) => {
-        console.log("Posts", response.posts);
-        this.posts = response.posts;
-        this.getComment();
-      }
+  getPosts(userId: number) {
+    return this.http
+      .get<{ posts: Post[] }>(`${this.ROOT_URL}/posts/user/${userId}`)
+      .pipe(map((response) => response.posts));
+  }
+
+  getCommentsForPosts(posts: Post[]) {
+    const commentRequests = posts.map((post) =>
+      this.http
+        .get<{ comments: Comment[] }>(`${this.ROOT_URL}/comments/post/${post.id}`)
+        .pipe(map((response) => ({ postId: post.id, comments: response.comments })))
     );
-  }
-  
 
-  getComment() {
-    this.posts.forEach((post) => {
-      this.http.get<{ comments: Comment[] }>(`${this.ROOT_URL}/comments/post/${post.id}`).subscribe(
-        (response) => {
-          this.comments[post.id] = response.comments;
-        }
-      );
+    forkJoin(commentRequests).subscribe((commentsData) => {
+      commentsData.forEach((data) => {
+        this.comments[data.postId] = data.comments;
+      });
     });
   }
 }
